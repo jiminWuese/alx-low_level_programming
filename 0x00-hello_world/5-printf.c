b94d1a4e985f38dad34e49e0c0c3061c21753782
Jimin Wuese <jiminwuese@gmail.com>
2022-12-08 11:00:58 -0800

    Hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,8 +8,9 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
 
-		return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+		return (0);
 
 }
