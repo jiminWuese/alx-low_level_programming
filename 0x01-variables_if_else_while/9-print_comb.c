4e69fd914f6a106a1f2ff1d976a84bfaefff504f
Jimin Wuese <jiminwuese@gmail.com>
2022-12-09 15:04:33 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,23 +7,17 @@
 
 int main(void)
 {
-	int i, j, k, l;
-
-	for (i = 0; i <= 9; i++) {
-		for (j = 0; j <= 9; j++) {
-			for (k = 0; k <= 9; k++) {
-				for (l = 0; l <= 9; l++) {
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-					putchar(l + '0');
-					putchar(',');
-					putchar(' ');
-				}
-			}
+	int n;
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n !=57)
+		{
+			putchar(',');
+			putchar(' ');
 		}
+		putchar('\n');
+		return (0);
 	}
-	putchar('\n');
-
-	return (0);
 }
+
