6e041d5be611f212eb58f5db0839d8cd5d28743d
Jimin Wuese <jiminwuese@gmail.com>
2022-12-09 14:38:09 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * Write a program that prints the lowercase alphabet in reverse, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+       	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return 0;
+}
