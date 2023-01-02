05c8f57b6db033934c132a75f5b1f1265f25ab53
Jimin Wuese <jiminwuese@gmail.com>
2023-01-02 03:33:24 -0800

    Correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,63 +1,33 @@
 /**
- *  * _memset - a function that fills
- *   *         memory with a constant byte
- *    *
- *     * @s: input pointer to char type
- *      *    represents the pointer to the
- *       *    block of memory to fill
- *        * @b: input variable of char type
- *         *    represents the character to
- *          *    fill s
- *           * @n: unsigned int variable
- *            *    the number of bytes to be filled
- *             *
- *              * Return: A pointer to the filled memory
- *               *         area @s
- *               */
+ * __memset - a function that fills
+ * memory with a constant byte
+ *
+ * @s: input pointer to char type
+ *  represents the pointer to the
+ *  block of memory to fill
+ *  b: input variable of char type
+ *  represents the character to
+ *  fills s
+ *  @n: unsigned int variable
+ *  the number of bytes of be filled
+ *
+ *  Return: A pointer to he filled memory
+ *  area @s
+ *
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-		/**
-		 * 	 * declare an unsigned int
-		 * 	 	 * because we are storing a
-		 * 	 	 	 * value that will always be
-		 * 	 	 	 	 * non-negative (zero or positive)
-		 * 	 	 	 	 	*/
-		unsigned int i;
+	/**
+	 * declare an unsigned int
+	 *  because we are storing a
+	 *  value that will always be
+	 *  non-negative (zero or positive)
+	 */
+	unsigned int i;
 
-			for (i = 0; i < n; i++)
-						s[i] = b;
+	for (i = 0; i < n; i++)
+		s[i] = b;
 
-				return (s);
-}/**
-    * _memset - a function that fills
-    *  *         memory with a constant byte
-    *   *
-    *    * @s: input pointer to char type
-    *     *    represents the pointer to the
-    *      *    block of memory to fill
-    *       * @b: input variable of char type
-    *        *    represents the character to
-    *         *    fill s
-    *          * @n: unsigned int variable
-    *           *    the number of bytes to be filled
-    *            *
-    *             * Return: A pointer to the filled memory
-    *              *         area @s
-    *              */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-		/**
-		 * 	 * declare an unsigned int
-		 * 	 	 * because we are storing a
-		 * 	 	 	 * value that will always be
-		 * 	 	 	 	 * non-negative (zero or positive)
-		 * 	 	 	 	 	*/
-		unsigned int i;
-
-			for (i = 0; i < n; i++)
-						s[i] = b;
-
-				return (s);
+	return (s);
 }
